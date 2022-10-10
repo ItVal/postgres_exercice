c41af4b3f9f6f7b70c20095b108f9c4a4704a66b
ItVal <valentinhonvj1@gmail.com>
2022-10-10 12:23:01 +0200

    remove one file

--- tAssignment.sql
+++ tAssignment.sql
@@ -1,14 +1,14 @@
 BEGIN TRANSACTION;
 CREATE TABLE  Assignment(
 	     assignment_id SERIAL,
-	     student_id INT REFERENCES staff (student_id),
-             course_code INT REFERENCES course (course_code),
-             grade VARCHAR(50) NOT NULL,
+	     student_id INT REFERENCES student (student_id),
+         course_code INT REFERENCES course (course_code),
+         grade VARCHAR(50) NOT NULL,
 	     PRIMARY KEY (assignment_id, student_id, course_code)
 );
-INSERT INTO Assignment (grade) VALUES ('Bac1') ;
-INSERT INTO Assignment (grade) VALUES ('Bac2') ;
-INSERT INTO Assignment (grade) VALUES ('Bac3') ;
-INSERT INTO Assignment (grade) VALUES ('Bac4') ;
-INSERT INTO Assignment (grade) VALUES ('Bac5') ;
+INSERT INTO Assignment (grade, student_id, course_code) VALUES ('Bac1', 1, 1) ;
+INSERT INTO Assignment (grade, student_id, course_code) VALUES ('Bac2', 2, 2) ;
+INSERT INTO Assignment (grade, student_id, course_code) VALUES ('Bac3', 3, 3) ;
+INSERT INTO Assignment (grade, student_id, course_code) VALUES ('Bac4', 5, 5 ) ;
+INSERT INTO Assignment (grade, student_id, course_code) VALUES ('Bac5', 5, 5) ;
 COMMIT;
--- tEnrollement.sql
+++ tEnrollement.sql
@@ -1,14 +1,14 @@
 BEGIN TRANSACTION;
-CREATE TABLE  Enrolement(
-	     student_id INT REFERENCES staff (student_id),
+CREATE TABLE  Enrollement(
+	     student_id INT REFERENCES student (student_id),
 	     course_code INT REFERENCES course (course_code),
 	     date_enrol DATE DEFAULT current_date,
 	     final_grade VARCHAR(50) NOT NULL,
 	     PRIMARY KEY (student_id, course_code)
 );
-INSERT INTO Enrolement (date_enrol, final_grade) VALUES ('10 Septembre 2021', 'Bac2') ;
-INSERT INTO Enrolement (date_enrol, final_grade) VALUES ('11 Septembre 2022', 'Bac1') ;
-INSERT INTO Enrolement (date_enrol, final_grade) VALUES ('19 Decembre 2021', 'Bac3') ;
-INSERT INTO Enrolement (date_enrol, final_grade) VALUES ('29 Janvier 2022', 'Bac4') ;
-INSERT INTO Enrolement (date_enrol, final_grade) VALUES ('05 Mars 2022', 'Bac5') ;
+INSERT INTO Enrollement (student_id, course_code, date_enrol, final_grade) VALUES (1, 1, '2021-09-10', 'Bac2') ;
+INSERT INTO Enrollement (student_id, course_code, date_enrol, final_grade) VALUES (2, 2, '2022-09-11', 'Bac1') ;
+INSERT INTO Enrollement (student_id, course_code, date_enrol, final_grade) VALUES (3, 3, '2021-12-19', 'Bac3') ;
+INSERT INTO Enrollement (student_id, course_code, date_enrol, final_grade) VALUES (4, 4, '2022-01-29', 'Bac4') ;
+INSERT INTO Enrollement (student_id, course_code, date_enrol, final_grade) VALUES (5, 5, '2022-03-05', 'Bac5') ;
 COMMIT;
